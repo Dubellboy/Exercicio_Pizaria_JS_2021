da8c2f2875f13dffb0ed2ec1d2f78b949b4df404
Eduardo Chagas <eduardo_cns@hotmail.com>
2021-04-11 11:13:10 -0300

    Projeto completo

--- script.js
+++ script.js
@@ -94,24 +94,91 @@ cs('.pizzaInfo--size').forEach ((size, Index)=>{//
 
 c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     let size =  parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
-
     let identifier = pizzaJson[modalKey].id+'@'+size;//identificador
-
     let key = cart.findIndex((item)=> item.identifier == identifier);
-    
-    if(key > -1) {
-        cart[key].qt += modalQt;
-    }else{
-        cart.push({
-            id:pizzaJson[modalKey].id,
-            size,
-            qt:modalQt
-            
-        });
-        ;
-   };
-
+        if(key > -1) {
+            cart[key].qt += modalQt;
+        }else{
+            cart.push({
+                identifier,
+                id:pizzaJson[modalKey].id,
+                size,
+                qt:modalQt
+            });      
+    };
+    updateCart();
     closeModal();
 });
+c('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0) {
+    c('aside').style.left = '0';
+    }     
+});
+c('.menu-closer').addEventListener('click', ()=>{//Fechar carrinho mobile
+    c('aside').style.left = '100vw';
+});
+
+function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length;//Atualizar quantidade no carrinho mobile
+
+    if(cart.length > 0) {
+        c('aside').classList.add('show');
+        c('.cart').innerHTML = '';//Zerar a lista do carrinho
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
+
+
+            let cartItem = c('.models .cart--item').cloneNode(true);
+
+            let pizzaSizeName; //Mostrar o tamanho da pizza escolhida no carrinho
+            switch(cart[i].size) {
+               case 0:
+                pizzaSizeName = 'P';
+                break; 
+                case 1:
+                pizzaSizeName = 'M';
+                break; 
+                case 2:
+                pizzaSizeName = 'G';
+                break; 
+            }
+
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;//Mostrar a imagem da pizza no carrinho
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName; // MOstrar o nome da pizza no carrinho
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                }else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++;
+                updateCart();
+            });
+
+            c('.cart').append(cartItem);
+        }
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+        c('.subtotal span:last-child').innerHTML = `RE ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `RE ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `RE ${total.toFixed(2)}`;
+
+    }else {
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
+    }
+}
 
 
